d2b8d49434fa3257da0833dcfb9fd89674320334
leeedohyun <ldh19738@gmail.com>
2023-12-28 23:29:53 +0900

    test: 우승자 찾는 기능 테스트

--- src/test/java/racingcar/model/RacingCarsTest.java
+++ src/test/java/racingcar/model/RacingCarsTest.java
@@ -41,4 +41,32 @@ class RacingCarsTest {
         // then
         assertThat(racingCars.getCars()).contains(new RacingCar("pobi", 0));
     }
+
+    @Test
+    void 우승자가_1명인_경우() {
+        // given
+        final List<RacingCar> cars = List.of(new RacingCar("pobi", 2), new RacingCar("woni", 1));
+        final RacingCars racingCars = new RacingCars(cars);
+
+        // when
+        final List<RacingCar> winners = racingCars.findWinners();
+
+        // then
+        assertThat(winners).hasSize(1)
+                .contains(new RacingCar("pobi", 2));
+    }
+
+    @Test
+    void 우승자가_여러_명인_경우() {
+        // given
+        final List<RacingCar> cars = List.of(new RacingCar("pobi", 2), new RacingCar("woni", 2));
+        final RacingCars racingCars = new RacingCars(cars);
+
+        // when
+        final List<RacingCar> winners = racingCars.findWinners();
+
+        // then
+        assertThat(winners).hasSize(2)
+                .contains(new RacingCar("pobi", 2), new RacingCar("woni", 2));
+    }
 }
