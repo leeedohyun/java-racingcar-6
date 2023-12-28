6713b14db286cfe7b6fbecc25fa2a0741bd0fe57
leeedohyun <ldh19738@gmail.com>
2023-12-28 21:18:18 +0900

    test: 전진 기능 테스트

--- src/test/java/racingcar/model/RacingCarTest.java
+++ src/test/java/racingcar/model/RacingCarTest.java
@@ -1,5 +1,6 @@
 package racingcar.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,5 +13,31 @@ class RacingCarTest {
     void 자동차의_이름이_4자를_초과하면_예외_발생(final String carName) {
         assertThatThrownBy(() -> new RacingCar(carName))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 8})
+    void 무작위_값이_4_이상인_경우_전진(final int random) {
+        // given
+        final RacingCar racingCar = new RacingCar("pobi");
+
+        // when
+        racingCar.move(random);
+
+        // then
+        assertThat(racingCar.getLocation()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void 무작위_값이_4_미만인_경우_정지(final int random) {
+        // given
+        final RacingCar racingCar = new RacingCar("pobi");
+
+        // when
+        racingCar.move(random);
+
+        // then
+        assertThat(racingCar.getLocation()).isEqualTo(0);
     }
 }
