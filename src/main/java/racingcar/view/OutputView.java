1dda2aab82db018723e5d225be4d453436dcfca5
leeedohyun <ldh19738@gmail.com>
2023-12-29 16:59:35 +0900

    feat: 각 차수별 실행 결과 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,10 +4,27 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import racingcar.model.RacingCar;
+import racingcar.model.RacingCars;
 
 public class OutputView {
 
     private static final String WINNER_DELIMITER = ", ";
+
+    public void printResultMessage() {
+        System.out.println("\n실행 결과");
+    }
+
+    public void printResult(final RacingCars racingCars) {
+        for (RacingCar car : racingCars.getCars()) {
+            System.out.print(car.getName() + " : ");
+            final StringBuilder result = new StringBuilder();
+            for (int i = 0; i < car.getLocation(); i++) {
+                result.append("-");
+            }
+            System.out.println(result);
+        }
+        System.out.println();
+    }
 
     public void printWinners(final List<RacingCar> winners) {
         System.out.print("최종 우승자 : ");
