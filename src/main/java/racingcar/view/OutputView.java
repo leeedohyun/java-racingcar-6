8b9b8af6bf603f7180ac49bbed2a4b13c47aa3b2
leeedohyun <ldh19738@gmail.com>
2023-12-29 16:44:17 +0900

    refactor: 우승자 이름 구분자 상수화

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,11 +7,13 @@ import racingcar.model.RacingCar;
 
 public class OutputView {
 
+    private static final String WINNER_DELIMITER = ", ";
+
     public void printWinners(final List<RacingCar> winners) {
         System.out.print("최종 우승자 : ");
         String result = winners.stream()
                 .map(RacingCar::getName)
-                .collect(Collectors.joining(", "));
+                .collect(Collectors.joining(WINNER_DELIMITER));
         System.out.println(result);
     }
 }
